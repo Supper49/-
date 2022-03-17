860a1495df993d1c436ccb436351ade51fbfeabb
Supper49 <95171616+Supper49@users.noreply.github.com>
2022-03-18 02:57:18 +0900

    Create mergeKLists.java

---
+++ mergeKLists.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+            ListNode ans = new ListNode(-9999);
+            for (int i =0;i<lists.length;i++){
+                ans =mergeTwoLists(lists[i],ans);
+            }
+            return ans.next;
+
+    }
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+            ListNode head = new ListNode();
+            ListNode l3 =head;
+            while(list1 !=null && list2!=null){
+                if(list1.val>=list2.val){
+                    l3.next = list2;
+                    l3 = l3.next;
+                    list2=list2.next;
+                }else{
+                    l3.next = list1;
+                    l3 = l3.next;
+                    list1=list1.next;
+                }
+            }
+
+            if (list1 == null) {
+                l3.next = list2;
+            } else {
+                l3.next = list1;
+            }
+                
+            return head.next; 
+        }
+}
